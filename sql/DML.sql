d54ede0c13bce057b8fb02b10002feff1e242465
Fauzan Fauzia Fauzi <fauzanfauziafauzi@gmail.com>
2026-07-14 14:50:32 +0700

    Menambahkan data kedalam tabel customers

---
+++ sql/DML.sql
@@ -0,0 +1,10 @@
+-- ========================================================
+-- DATABASE: SYSTEM TIKET TRAVEL
+-- ========================================================
+
+INSERT INTO customers (name, phone, email, address) VALUES
+('abcd', '08---', 'abcd@gmail.com', 'Jl. Kota Bandung'),
+('abcd', '08---', 'abcd@yahoo.com', 'Jl. Kota Bandung'),
+('abcd', '08---', 'abcd@outlook.com', 'Jl. kota Bandung'),
+('abcd', '08---', 'abcd@gmail.com', 'Jl. Kota Bandung'),
+('abcd', '08---', 'abcd@gmail.com', 'Jl. Kota Bogor');
