9225a655b8c34a8badc7bf95c7c002dfcf7777d0
Fauzan Fauzia Fauzi <fauzanfauziafauzi@gmail.com>
2026-07-14 14:36:16 +0700

    Query Tabel Armadas

--- sql/DDL.sql
+++ sql/DDL.sql
@@ -8,4 +8,12 @@ CREATE TABLE customers (
     name VARCHAR(100) NOT NULL,
     phone VARCHAR(15) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL
-) ENGINE=InnoDB;
+) 
+
+-- Membuat Tabel Armadas
+CREATE TABLE armadas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    model VARCHAR(50) NOT NULL, -- Contoh: Toyota Hiace, Isuzu Elf
+    capacity INT NOT NULL,
+    status ENUM('active', 'maintenance', 'inactive') DEFAULT 'active'
+) 
