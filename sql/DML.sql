b6e48ea0893b65a6ccd6523a8ee2547b3d0449ed
Fauzan Fauzia Fauzi <fauzanfauziafauzi@gmail.com>
2026-07-17 14:59:18 +0700

    JOIN Lengkap Semua Tabel Berelasi (5 Tabel)

--- sql/DML.sql
+++ sql/DML.sql
@@ -65,4 +65,19 @@ SELECT
     s.departure_time AS 'Jam Keberangkatan'
 FROM tickets AS t
 JOIN bookings AS b ON t.booking_id = b.id
-JOIN schedules AS s ON t.schedule_id = s.id; 
+JOIN schedules AS s ON t.schedule_id = s.id;
+
+-- 3. JOIN Lengkap Semua Tabel Berelasi (5 Tabel)
+SELECT 
+    t.passenger_name AS 'Nama Penumpang',
+    t.seat_number AS 'Nomor Kursi',
+    b.booking_code AS 'Kode Booking',
+    r.departure_city AS 'Kota Asal',
+    r.arrival_city AS 'Kota Tujuan',
+    s.departure_date AS 'Tanggal Keberangkatan',
+    a.model AS 'Armada Kendaraan'
+FROM tickets AS t
+JOIN bookings AS b ON t.booking_id = b.id
+JOIN schedules AS s ON t.schedule_id = s.id
+JOIN routes AS r ON s.route_id = r.id
+JOIN armadas AS a ON s.armada_id = a.id;
