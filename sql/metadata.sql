98ba82f68de25f58238b577f1470818b701e35a0
Fauzan Fauzia Fauzi <fauzanfauziafauzi@gmail.com>
2026-07-17 14:53:15 +0700

    Melihat Semua Tabel di Database

--- sql/metadata.sql
+++ sql/metadata.sql
@@ -1,3 +1,7 @@
--- ========================================================
--- DATABASE: SYSTEM TIKET TRAVEL
--- ========================================================
+-- =======================================================
+-- KELOMPOK 2 - DATABASE FUNDAMENTAL
+-- FILE: metadata.sql (Pengecekan Struktur & Metadata Tabel)[cite: 1]
+-- =======================================================
+
+-- 1. Melihat Semua Tabel di Database[cite: 1]
+SHOW TABLES;
