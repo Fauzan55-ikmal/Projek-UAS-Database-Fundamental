678d44c2cf73491e4801260fc76481fd4feca1fb
Fauzan Fauzia Fauzi <fauzanfauziafauzi@gmail.com>
2026-07-17 14:41:50 +0700

    Query untuk membuat tabel (CREATE TABLE)

--- sql/DDL.sql
+++ sql/DDL.sql
@@ -6,43 +6,38 @@
 CREATE DATABASE IF NOT EXISTS db_travel_kel2;
 USE db_travel_kel2;
 
--- Membuat Tabel Customers
+-- 2. Membuat Tabel-Tabel Awal (Sederhana)
 CREATE TABLE customers (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     phone VARCHAR(15) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL
-) 
+);
 
--- Membuat Tabel Armadas
 CREATE TABLE armadas (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    model VARCHAR(50) NOT NULL, -- Contoh: Toyota Hiace, Isuzu Elf
+    model VARCHAR(50) NOT NULL,
     capacity INT NOT NULL,
     status ENUM('active', 'maintenance', 'inactive') DEFAULT 'active'
-) 
+);
 
--- Membuat Tabel Routes
 CREATE TABLE routes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     departure_city VARCHAR(50) NOT NULL,
     arrival_city VARCHAR(50) NOT NULL,
-    duration TIME NOT NULL, -- Contoh: '02:30:00' artinya 2 jam 30 menit
-) 
+    duration TIME NOT NULL
+);
 
--- Membuat Tabel Schedules
 CREATE TABLE schedules (
     id INT AUTO_INCREMENT PRIMARY KEY,
     departure_date DATE NOT NULL,
     departure_time TIME NOT NULL,
-    price DECIMAL(10, 2) NOT NULL -- Harga dinamis menyesuaikan waktu operasional
-) 
+    price DECIMAL(10, 2) NOT NULL
+);
 
--- Membuat Tabel Bookings
 CREATE TABLE bookings (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    booking_code VARCHAR(12) UNIQUE NOT NULL, -- Kode unik transaksi di invoice
+    booking_code VARCHAR(12) UNIQUE NOT NULL,
     total_amount DECIMAL(10, 2) NOT NULL,
     payment_status ENUM('pending', 'paid', 'cancelled') DEFAULT 'pending'
-)
-
+);
