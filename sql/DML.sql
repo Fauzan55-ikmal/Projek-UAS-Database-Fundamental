906d809b13358aa5fdb3b66c65478f9070af740c
Fauzan Fauzia Fauzi <fauzanfauziafauzi@gmail.com>
2026-07-19 17:43:16 +0700

    Query DML Fix

--- sql/DML.sql
+++ sql/DML.sql
@@ -1,5 +1,5 @@
 -- =================================================================
--- FASE 4: DATA MANIPULATION LANGUAGE (DML) - SYSTEM TIKET TRAVEL
+-- DATA MANIPULATION LANGUAGE (DML) - SYSTEM TIKET TRAVEL
 -- =================================================================
 
 -- -----------------------------------------------------------------
@@ -38,10 +38,9 @@ INSERT INTO bookings (booking_code, customer_id, total_amount, payment_status, p
 
 -- Memasukkan 3 Data Tiket Awal
 INSERT INTO tickets (booking_id, schedule_id, passenger_name, passenger_id_card, seat_number) VALUES
-(1, 1, 'Septi', '3204123456780001', 5),
-(1, 1, 'Teman Septi', '3204123456780002', 6),
-(2, 3, 'Rizky', '3204987654320001', 3);
-
+(1, 1, 'Angga Saputra', '3204123456780001', 5),
+(1, 1, 'Fauzan Fauzia Fauzi', '3204123456780002', 6),
+(2, 3, 'Udin Berlin', '3204987654320001', 3);
 
 -- -----------------------------------------------------------------
 -- 2. TAHAP PEMBARUAN & PENAMBAHAN DATA (MEMENUHI SYARAT MINIMAL 5 DATA)
@@ -49,34 +48,34 @@ INSERT INTO tickets (booking_id, schedule_id, passenger_name, passenger_id_card,
 
 -- Menambahkan data pelanggan ke-4 dan ke-5
 INSERT INTO customers (name, phone, email, address) VALUES
-('Fauzan', '089988776655', 'fauzan@tedc.ac.id', 'Kopo, Bandung'),
-('Dani', '081322446688', 'dani@tedc.ac.id', 'Cileunyi, Bandung');
+('Juni', '089988776655', 'juniatun@tbk.ac.id', 'Kopo, Bandung'),
+('Ranti', '081322446688', 'ranti@tbk.ac.id', 'Cileunyi, Bandung');
+
 
 -- Menambahkan data armada ke-4 dan ke-5
 INSERT INTO armadas (plate_number, model, capacity, status) VALUES
-('D 4455 AB', 'Toyota Hiace Premio', 11, 'active'),
+('D 4455 AB', 'Toyota Hiace Premio', 12, 'active'),
 ('D 7788 XYZ', 'Isuzu Elf', 19, 'inactive');
 
 -- Menambahkan data rute ke-4 dan ke-5
 INSERT INTO routes (departure_city, arrival_city, duration, base_price) VALUES
 ('Bandung', 'Surabaya', '10:00:00', 450000.00),
-('Bandung', 'Sukabumi', '03:00:00', 100000.00);
+('Bandung', 'Sukabumi', '01:30:00', 85000.00);
 
 -- Menambahkan data jadwal ke-4 dan ke-5
 INSERT INTO schedules (route_id, armada_id, departure_date, departure_time, price) VALUES
 (3, 4, '2026-07-21', '20:00:00', 290000.00),
-(5, 1, '2026-07-22', '10:00:00', 110000.00);
+(5, 1, '2026-07-22', '10:00:00', 85000.00);
 
 -- Menambahkan data booking ke-4 dan ke-5
 INSERT INTO bookings (booking_code, customer_id, total_amount, payment_status, payment_method) VALUES
-('TRX-20260004', 4, 110000.00, 'paid', 'QRIS'),
-('TRX-20260005', 5, 160000.00, 'cancelled', 'Transfer Bank');
+('TRX-20260004', 4, 290000.00, 'paid', 'QRIS'),
+('TRX-20260005', 5, 85000.00, 'cancelled', 'Transfer Bank');
 
 -- Menambahkan data tiket ke-4 dan ke-5
 INSERT INTO tickets (booking_id, schedule_id, passenger_name, passenger_id_card, seat_number) VALUES
-(3, 4, 'Rafael', '3204112233440001', 1),
-(4, 5, 'Fauzan', '3204998877660001', 2);
-
+(6, 1, 'Juni', '3204112233440001', 1),
+(7, 2, 'Ranti', '3204998877660001', 2);
 
 -- -----------------------------------------------------------------
 -- 3. TAHAP MANIPULASI INTERAKTIF (UPDATE & DELETE OPERASIONAL)
@@ -100,49 +99,12 @@ WHERE plate_number = 'D 9999 WT';
 -- [UPDATE - Skenario 4]: Pelanggan melakukan update nomor HP baru di sistem
 UPDATE customers 
 SET phone = '081399998888' 
-WHERE name = 'Dani';
+WHERE name = 'Ranti';
 
 -- [DELETE - Skenario 1]: Uji coba ON DELETE CASCADE (Batal booking otomatis hapus manifest tiket)
 DELETE FROM bookings 
-WHERE booking_code = 'TRX-20260001';
+WHERE booking_code = 'TRX-20260003';
 
 -- [DELETE - Skenario 2]: Uji coba ON DELETE RESTRICT (Proteksi rute agar tidak bisa dihapus jika jadwal aktif)
 DELETE FROM routes 
-WHERE id = 5;
-
--- =======================================================
--- 4. READ DATA / REPORT (Simulasi JOIN & AS Alias)
--- =======================================================
-
--- 1. JOIN Tiket dengan Booking (2 Tabel)
-SELECT 
-    t.passenger_name AS 'Nama Penumpang',
-    b.booking_code AS 'Kode Booking',
-    b.payment_status AS 'Status Pembayaran'
-FROM tickets AS t
-JOIN bookings AS b ON t.booking_id = b.id;
-
--- 2. JOIN Tiket, Booking, dan Jadwal (3 Tabel)
-SELECT 
-    t.passenger_name AS 'Nama Penumpang',
-    b.booking_code AS 'Kode Booking',
-    s.departure_date AS 'Tanggal Pergi',
-    s.departure_time AS 'Jam Keberangkatan'
-FROM tickets AS t
-JOIN bookings AS b ON t.booking_id = b.id
-JOIN schedules AS s ON t.schedule_id = s.id;
-
--- 3. JOIN Lengkap Semua Tabel Berelasi (5 Tabel)
-SELECT 
-    t.passenger_name AS 'Nama Penumpang',
-    t.seat_number AS 'Nomor Kursi',
-    b.booking_code AS 'Kode Booking',
-    r.departure_city AS 'Kota Asal',
-    r.arrival_city AS 'Kota Tujuan',
-    s.departure_date AS 'Tanggal Keberangkatan',
-    a.model AS 'Armada Kendaraan'
-FROM tickets AS t
-JOIN bookings AS b ON t.booking_id = b.id
-JOIN schedules AS s ON t.schedule_id = s.id
-JOIN routes AS r ON s.route_id = r.id
-JOIN armadas AS a ON s.armada_id = a.id;
+WHERE id = 4;
